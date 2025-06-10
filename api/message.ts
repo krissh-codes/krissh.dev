5e750396f57c6c0c990faec11466a00e9a64cea4
krissh-the-dev <akrishnamoorthy007@gmail.com>
2025-06-10 22:19:10 +0530

    refactor: moved constructResponse util function to ResponseBuilder util

--- api/message.ts
+++ api/message.ts
@@ -1,17 +1,11 @@
 import type { MessagePayload } from '@common-types';
-
-function constructResponse(statusCode: number, responseMessage: string, details?: unknown) {
-    return new Response(
-        JSON.stringify({ status: statusCode >= 400 ? 'error' : 'success', message: responseMessage, details }),
-        { status: statusCode }
-    );
-}
+import ResponseBuilder from './utils/ResponseBuilder';
 
 export default async function MessageController(req: Request) {
     if (req.method !== 'POST') {
-        return constructResponse(405, 'method not allowed');
+        return ResponseBuilder.constructResponse(405, 'method not allowed');
     }
 
     const body = (await req.json()) as MessagePayload;
-    return constructResponse(200, 'message sent successfully', body);
+    return ResponseBuilder.constructResponse(200, 'message sent successfully', body);
 }
---
+++ api/utils/ResponseBuilder.ts
@@ -0,0 +1,8 @@
+export default class ResponseBuilder {
+    static constructResponse(statusCode: number, responseMessage: string, details?: unknown) {
+        return new Response(
+            JSON.stringify({ status: statusCode >= 400 ? 'error' : 'success', message: responseMessage, details }),
+            { status: statusCode }
+        );
+    }
+}
