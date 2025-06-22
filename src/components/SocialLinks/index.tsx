3f71959f9f6ae3e1f8225c91918f41ba550c0db3
krissh-the-dev <akrishnamoorthy007@gmail.com>
2025-06-22 19:34:37 +0530

    feat: added handle redirects

--- src/components/SocialLinks/index.tsx
+++ src/components/SocialLinks/index.tsx
@@ -5,23 +5,23 @@ import classes from './social-links.module.scss';
 export default function SocialLinks() {
     return (
         <div className={classes.socials}>
-            <HyperLink to="https://bsky.app/profile/krissh.dev" noUnderLine lone style={{ margin: 0 }}>
+            <HyperLink to="/bluesky" noUnderLine lone style={{ margin: 0 }}>
                 <TbBrandBluesky />
             </HyperLink>
 
-            <HyperLink to="https://x.com/krissh_tweets" noUnderLine lone style={{ margin: 0 }}>
+            <HyperLink to="/x" noUnderLine lone style={{ margin: 0 }}>
                 <TbBrandTwitter />
             </HyperLink>
 
-            <HyperLink to="https://linkedin.com/in/krissh-connects" noUnderLine lone style={{ margin: 0 }}>
+            <HyperLink to="/linkedin" noUnderLine lone style={{ margin: 0 }}>
                 <TbBrandLinkedin />
             </HyperLink>
 
-            <HyperLink to="https://reddit.com/user/krissh_speaks" noUnderLine lone style={{ margin: 0 }}>
+            <HyperLink to="/reddit" noUnderLine lone style={{ margin: 0 }}>
                 <TbBrandReddit />
             </HyperLink>
 
-            <HyperLink to="https://instagram.com/krissh_posts" noUnderLine lone style={{ margin: 0 }}>
+            <HyperLink to="/instagram" noUnderLine lone style={{ margin: 0 }}>
                 <TbBrandInstagram />
             </HyperLink>
         </div>
