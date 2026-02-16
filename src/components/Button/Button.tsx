9138ca1d8437058ee2168d1e0ea6f9312e16cbe3
krissh-codes <akrishnamoorthy007@gmail.com>
2026-02-16 08:36:27 +0530

    fix(a11y): improved button accessibility

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,14 +1,25 @@
-import type { ButtonHTMLAttributes, DetailedHTMLProps, MouseEvent, MouseEventHandler } from 'react';
+import type { ButtonHTMLAttributes, DetailedHTMLProps, MouseEventHandler, ReactNode } from 'react';
 import classes from './button.module.scss';
 
-export function Button({ onClick, link, children, variant, target }: ButtonProps) {
-    const openLink = (e: MouseEvent<HTMLButtonElement>) => {
-        e.preventDefault();
-        window.open(link, `_${target ?? 'self'}`);
-    };
+export function Button({ onClick, link, children, variant, target, type, disabled }: ButtonProps) {
+    const classesName = `${classes.button} ${variant === 'sm' ? classes.button__small : classes.button__medium}`;
+
+    if (link) {
+        return (
+            <a
+                href={link}
+                target={`_${target ?? 'self'}`}
+                rel={target === 'blank' ? 'noopener noreferrer' : undefined}
+                className={classesName}
+                aria-disabled={disabled}
+            >
+                {children}
+            </a>
+        );
+    }
 
     return (
-        <button className={`${classes.button} ${variant === 'sm' ? classes.button__small : classes.button__medium}`} onClick={link ? openLink : onClick}>
+        <button className={classesName} onClick={onClick} type={type ?? 'button'} disabled={disabled}>
             {children}
         </button>
     );
@@ -17,6 +28,7 @@ export function Button({ onClick, link, children, variant, target }: ButtonProps
 interface ButtonProps extends DetailedHTMLProps<ButtonHTMLAttributes<HTMLButtonElement>, HTMLButtonElement> {
     link?: string;
     variant?: 'sm' | 'md';
-    target?: 'self' | 'blank'
+    target?: 'self' | 'blank';
+    children: ReactNode;
     onClick?: MouseEventHandler<HTMLButtonElement>;
 }
